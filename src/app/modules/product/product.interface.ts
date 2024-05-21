90264b84610c6ad165298439bb30a9410910075c
NabilHassan7 <fakrulibnehassan@gmail.com>
2024-05-22 00:11:04 +0600

    declared custom static method for product

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -1,13 +1,19 @@
+// importing from mongoose
+import { Model } from "mongoose";
+
+// type declaration for variants
 export type TVariants = {
     type : string;
     value : string;
 }
 
+// type declaration for inventory
 export type TInventory = {
     quantity : string;
     inStock : boolean;
 }
 
+// type declaration for product
 export type TProduct = {
     id : string;
     name : string;
@@ -17,4 +23,9 @@ export type TProduct = {
     tags : string[];
     variants : TVariants[];
     inventory : TInventory;
+}
+
+// custom static method for product
+export interface ProductModel extends Model<TProduct> {
+    isProductExists(id : string): Promise<TProduct | null>
 }
