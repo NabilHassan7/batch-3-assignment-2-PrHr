c896740ec8c66ef43f5fcbba91edde2150718813
NabilHassan7 <fakrulibnehassan@gmail.com>
2024-05-22 00:00:26 +0600

    created initial product interface

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,20 @@
+export type TVariants = {
+    type : string;
+    value : string;
+}
+
+export type TInventory = {
+    quantity : string;
+    inStock : boolean;
+}
+
+export type TProduct = {
+    id : string;
+    name : string;
+    description : string;
+    price : number;
+    category : string;
+    tags : string[];
+    variants : TVariants[];
+    inventory : TInventory;
+}
