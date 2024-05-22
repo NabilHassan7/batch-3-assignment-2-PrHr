ab1153d93c4dfd997907b2ac81f68348eab68c48
NabilHassan7 <fakrulibnehassan@gmail.com>
2024-05-22 12:36:58 +0600

    added middleware to handle if route not found

--- src/app.ts
+++ src/app.ts
@@ -18,15 +18,23 @@ app.use(express.json());
 // cors
 app.use(cors());
 
-// application routes
+// application product routes end point
 app.use('/api/products', ProductRoutes);
 
-// application routes
+// application order routes end point
 app.use('/api/orders', OrderRoutes);
 
 // home route
 app.get('/', (req: Request, res: Response) => {
   res.send('API Server for Assignment-2 by NabilHassan7')
+})
+
+// catch all middleware
+app.use((req : Request, res : Response) => {
+  res.status(404).json({
+    success : false,
+    message : "Route not found"
+  })
 })
 
 export default app
